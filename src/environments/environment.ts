ef2316ddd038f50021350750b03c00f6c0a032b2
Nespire <33282623+Nespire@users.noreply.github.com>
2020-12-05 00:28:12 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyD5gF4BWpcamXEuND93HuXY6BeRH6GstAc",
-    authDomain: "angularfirebasecrud-7c726.firebaseapp.com",
-    databaseURL: "https://angularfirebasecrud-7c726.firebaseio.com",
-    projectId: "angularfirebasecrud-7c726",
-    storageBucket: "angularfirebasecrud-7c726.appspot.com",
-    messagingSenderId: "499153951315",
-    appId: "1:499153951315:web:e3d5b500b64c35728684a6",
-    measurementId: "G-C2WL35PQZ9"
-  }
+  production: false
 };
 
 /*
